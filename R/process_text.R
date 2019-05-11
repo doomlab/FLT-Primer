a293cf1cbde39ade5d00d97d876557be3c088987
Dr. Erin M. Buchanan <buchananlab@gmail.com>
2019-05-11 13:19:57 -0400

    working on breaking down multiwords

--- R/process_text.R
+++ R/process_text.R
@@ -46,22 +46,113 @@ for (i in 1:nrow(spell_checked)){
 
 lemmas <- spell_checked
 
-### Loop over the data.frame
-for (i in 1:nrow(lemmas)){
-  ### Replace with lemmas
-  stem_suggest <- hunspell_stem(unlist(hunspell_parse(lemmas$answer[i])))
-  lemmas$answer[i] <- paste0(unlist(lapply(stem_suggest, tail, n = 1L)), collapse = " ")
-}
-
 ## Install the koRpus package
 #install.packages("koRpus")
+#install.packages("koRpus.lang.en")
+#install.packages("tokenizers")
 
+## You must load both packages separately
 library(koRpus)
+library(koRpus.lang.en)
+library(tokenizers)
+
+## Install TreeTagger 
+#https://www.cis.uni-muenchen.de/~schmid/tools/TreeTagger/
+
+## Find all types for faster lookup
+all_answers <- paste0(lemmas$answer, collapse = " ")
+all_answers <- unlist(tokenize_words(all_answers))
+all_answers <- unique(all_answers)
+
+## This function has both suppressWarnings & suppressMessages
+## You should first view these to ensure proper processing
+temp_tag <- suppressWarnings(
+  suppressMessages(
+    ## Note: the NULL option is to control for the <unknown> that appears
+    ## to occur with the last word in each text
+    treetag(c(all_answers, "NULL"), 
+            ## Control the parameters of treetagger
+            treetagger="manual", format="obj",
+            TT.tknz=FALSE, lang="en",
+            TT.options=list(path="~/TreeTagger", preset="en"))))
+
+## Remove all tags not using
+replacement_lemmas <- temp_tag@TT.res
+replacement_lemmas <- subset(replacement_lemmas, 
+                             #unknown values
+                             lemma != "<unknown>" & 
+                             #numbers
+                             lemma!= "@card@" & 
+                             #token should change more than case
+                             token != tolower(lemma)) 
+
+## Install the stringi package
+#install.packages("stringi")
+
+## Replace all the original tokens with new lemmas
+lemmas$answer <- stri_replace_all_regex(lemmas$answer, 
+                       pattern = paste("\\b", replacement_lemmas$token, "\\b", sep = ""),
+                       replacement = replacement_lemmas$lemma,
+                       F, list(case_insensitive = TRUE))
+
+# Stopwords and Other Exclusions ------------------------------------------
+
+no_stop <- lemmas
+
+## Install the stopwords package or use tm
+#install.packages("stopwords")
+
+## Exclude all stopwords
+no_stop$answer <- stri_replace_all_regex(no_stop$answer, 
+                                        pattern = paste(stopwords(language = "en", 
+                                                                  source = "snowball"), 
+                                                        collapse = "\\b|\\b"),
+                                        replacement = "",
+                                        F, list(case_insensitive = TRUE))
 
 
+# Bag of Words ------------------------------------------------------------
 
-tagged.results <- treetag(c("wings", "run", "ran", "cheese"), 
-                          treetagger="manual", format="obj",
-                          TT.tknz=FALSE, lang="en",
-                          TT.options=list(path="~/TreeTagger", preset="en"))
-tagged.results@TT.res
+bag_words <- data.frame(Word=character(),
+                        Feature=character(), 
+                        Frequency=numeric(), 
+                        stringsAsFactors=FALSE) 
+
+unique_concepts <- unique(no_stop$word)
+
+## Loop over each word
+for (i in 1:length(unique_concepts)){
+
+  ## Create a table of frequencies
+  word_table <- as.data.frame(table(
+    unlist(
+      ## Tokenize the words
+      tokenize_words(
+        ## Put all answers together in one character string
+        paste0(no_stop$answer[no_stop$word == unique_concepts[i]], collapse = " ")))))
+  
+  ## Clean up the table
+  word_table$Word <- unique_concepts[i]
+  colnames(word_table) = c("Feature", "Frequency", "Word")
+  
+  bag_words <- rbind(bag_words, word_table[ , c(3, 1, 2)])
+  
+}
+
+# Multi-Word Strings -----------------------------------------------------
+
+#looks like VB NN
+# maybe VB JJ NN
+
+temp = treetag(c(lemmas$answer[i], "NULL"), 
+        ## Control the parameters of treetagger
+        treetagger="manual", format="obj",
+        TT.tknz=FALSE, lang="en",
+        TT.options=list(path="~/TreeTagger", preset="en"))
+View(temp@TT.res)
+
+
+##process the lemma text
+##take out determiners, conjunctions, preps, comma, punctuation 
+
+
